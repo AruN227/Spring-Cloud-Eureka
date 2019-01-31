5e7a242d84657b865af2cf3063a3fd307e289998
AruN227 <arunsakthi1923@gmail.com>
2019-01-31 12:55:21 +0530

    commit1

---
+++ spring-eureka-client-student-service/src/main/java/com/ak/webservices/springeurekaclientstudentservice/bean/Student.java
@@ -0,0 +1,26 @@
+package com.ak.webservices.springeurekaclientstudentservice.bean;
+
+public class Student {
+
+	private String name;
+	private String department;
+	public Student(String name, String department) {
+		super();
+		this.name = name;
+		this.department = department;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+	
+}
